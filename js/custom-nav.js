2d93180ea59c0af6669501bdf38cac174c23fe7c
developerDoor <dothebestmoon1016@gmail.com>
2024-10-08 19:41:36 +0900

    fix: 네비게이션바 호버시 bold처리

--- js/custom-nav.js
+++ js/custom-nav.js
@@ -9,17 +9,17 @@ class CustomNav extends HTMLElement {
               </a>
 
               <ul class="js-clone-nav d-none d-lg-inline-noone text-left site-menu">
-                <li><a href="index.html" style="font-size: 20px;">Home</a></li>
+                <li><a href="index.html" style="font-size: 20px;" onmouseover="this.style.fontWeight='bold'" onmouseout="this.style.fontWeight=''">Home</a></li>
                 <li class="has-children">
-                  <a href="/archive.html" style="font-size: 20px;">Archive</a>
+                  <a href="/archive.html" style="font-size: 20px;" onmouseover="this.style.fontWeight='bold'" onmouseout="this.style.fontWeight=''">Archive</a>
                   <ul class="dropdown">
-                    <li><a href="project1.html" style="font-size: 15px; line-height: 1">Project 1</a></li>
-                    <li><a href="project2.html" style="font-size: 15px; line-height: 1">Project 2</a></li>
-                    <li><a href="project3.html" style="font-size: 15px; line-height: 1">Project 3</a></li>
+                    <li><a href="project1.html" style="font-size: 15px; line-height: 1" onmouseover="this.style.fontWeight='bold'" onmouseout="this.style.fontWeight=''">Project 1</a></li>
+                    <li><a href="project2.html" style="font-size: 15px; line-height: 1" onmouseover="this.style.fontWeight='bold'" onmouseout="this.style.fontWeight=''">Project 2</a></li>
+                    <li><a href="project3.html" style="font-size: 15px; line-height: 1" onmouseover="this.style.fontWeight='bold'" onmouseout="this.style.fontWeight=''">Project 3</a></li>
                   </ul>
                 </li>
-                <li><a href="about.html" style="font-size: 20px;">About</a></li>
-                <li><a href="contact.html" style="font-size: 20px;">Contact</a></li>
+                <li><a href="about.html" style="font-size: 20px;" onmouseover="this.style.fontWeight='bold'" onmouseout="this.style.fontWeight=''">About</a></li>
+                <li><a href="contact.html" style="font-size: 20px;" onmouseover="this.style.fontWeight='bold'" onmouseout="this.style.fontWeight=''">Contact</a></li>
               </ul>
 
               <a
